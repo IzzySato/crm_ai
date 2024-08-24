057e9d12e1b0b188ba39e99e1d26d08de193ded0
IzzySato <izzy.sato@gmail.com>
2024-08-24 09:52:27 -0700

    fixed CORS origin

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from .routes import main
 from config import config
 from flask_cors import CORS
+import os
 
 def create_app(config_name='default'):
   app = Flask(__name__)
   CORS(app, supports_credentials=True, resources={
     r"/*": {
-        "origins": "http://localhost:3000",
+        "origins": os.getenv("REACT_URL", "http://localhost:3000"),
         "methods": ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
         "allow_headers": ["X-Requested-With", "Content-Type", "Authorization"],
         "supports_credentials": True
